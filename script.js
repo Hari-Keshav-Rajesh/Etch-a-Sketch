06ac38a5758535bd6f7d31cb7f5bed79419cd098
Hari-Keshav-Rajesh <harikeshav.rajesh@gmail.com>
2023-06-19 16:34:16 +0530

    Grid created and size change feature added

---
+++ script.js
@@ -0,0 +1,43 @@
+function sizeChange(){
+    size=prompt("Enter new size");
+    grid.innerHTML='';
+    for(let i=0;i<size;i++){
+        let column=document.createElement("div");
+        column.classList.add("column");
+        for(let j=0;j<size;j++){
+            let row=document.createElement("div");
+            row.style.cssText="width:50px;height:50px;";
+            row.style.border="thin double red"
+            row.classList.add("square");
+            column.appendChild(row);
+        }
+        column.style.display="flex";
+        column.style.justifyContent="center";
+        column.style.alignItems="center";
+        grid.appendChild(column);
+    }
+
+
+}
+
+let size=16;
+const changeSize=document.querySelector(".prompt");
+changeSize.addEventListener("click",sizeChange);
+
+const grid = document.querySelector(".grid");
+
+for(let i=0;i<16;i++){
+    let column=document.createElement("div");
+    column.classList.add("column");
+    for(let j=0;j<16;j++){
+        let row=document.createElement("div");
+        row.style.cssText="width:50px;height:50px;";
+        row.style.border="thin double red"
+        row.classList.add("square");
+        column.appendChild(row);
+    }
+    column.style.display="flex";
+    column.style.justifyContent="center";
+    column.style.alignItems="center";
+    grid.appendChild(column);
+}
